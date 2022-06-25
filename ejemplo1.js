519a9462c75ec7714859aa1b813c779526033a50
cfmaca18 <cfchocue@misena.edu.co>
2022-06-24 20:33:09 -0500

    segundo commit

--- ejemplo1.js
+++ ejemplo1.js
@@ -1,3 +1,14 @@
 class Libros {
-
+    constructor(autor, nombre, numeropaginas) {
+        this.autor = autor;
+        this.nombre = nombre;
+        this.numeropaginas = numeropaginas;
+    }
 }
+
+let libro1 = new Libros("Gabriel Garcia Marquez", "Cien años de soledad", "350");
+console.log(libro1);
+let libro2 = new Libros("Homero", "Hiliada", "500");
+console.log(libro2);
+let libro3 = new Libros("Homero", "La odisea", "500");
+console.log(libro3);
